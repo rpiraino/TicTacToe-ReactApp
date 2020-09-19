44cff71735b0ba842026a9d73f5d5b701d4719d0
rpiraino <70352162+rpiraino@users.noreply.github.com>
2020-09-19 13:28:35 -0400

    Add files via upload

---
+++ header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+       <header>
+ <div class="container">
+     <div id="branding">
+         <h1><span class="highlight">RWU Comsc230</span> Tic Tac Toe</h1>
+     </div>	
+     
+</div>
+     </header>
+    </div>
+  )
+}
+
+export default Header
